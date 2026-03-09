99b3b6f6dd1afdea7b485efb8f5d7a7cef07ca0c
Youssef-YN <nazihyoussef05@gmail.com>
2026-03-09 09:14:26 +0000

    deleting products works

--- src/main/java/youssef/nazih/springmvc/controller/ProductController.java
+++ src/main/java/youssef/nazih/springmvc/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import youssef.nazih.springmvc.repository.ProductRepository;
 
 @Controller
@@ -17,4 +18,12 @@ public class ProductController {
         model.addAttribute("productList", productRepository.findAll());
         return "products";
     }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name = "id") Long id) {
+        productRepository.deleteById(id);
+        return "redirect:/index";
+    }
+
+
 }
