2ff1109db6fcdab97746e8e2ef2aa07744d46ca6
Youssef-YN <nazihyoussef05@gmail.com>
2026-03-30 00:21:57 +0100

    got navbar working

--- src/main/java/youssef/nazih/springmvc/config/SecurityConfig.java
+++ src/main/java/youssef/nazih/springmvc/config/SecurityConfig.java
@@ -12,31 +12,31 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-        return new InMemoryUserDetailsManager(
-                User.withUsername("user1").password(passwordEncoder().encode("1234")).roles("USER").build(),
-                User.withUsername("user2").password(passwordEncoder().encode("1234")).roles("USER").build(),
-                User.withUsername("admin").password(passwordEncoder().encode("1234")).roles("USER", "ADMIN").build()
-        );
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .formLogin(Customizer.withDefaults())
-                .authorizeHttpRequests(ar -> ar.requestMatchers("/index/**").hasRole("USER"))
-                .authorizeHttpRequests(ar -> ar.requestMatchers("/save/**", "/delete/**").hasRole("USER"))
-                .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
-                .build();
-    }
-}
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+//        return new InMemoryUserDetailsManager(
+//                User.withUsername("user1").password(passwordEncoder().encode("1234")).roles("USER").build(),
+//                User.withUsername("user2").password(passwordEncoder().encode("1234")).roles("USER").build(),
+//                User.withUsername("admin").password(passwordEncoder().encode("1234")).roles("USER", "ADMIN").build()
+//        );
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .formLogin(Customizer.withDefaults())
+//                .authorizeHttpRequests(ar -> ar.requestMatchers("/index/**").hasRole("USER"))
+//                .authorizeHttpRequests(ar -> ar.requestMatchers("/save/**", "/delete/**").hasRole("USER"))
+//                .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
+//                .build();
+//    }
+//}
